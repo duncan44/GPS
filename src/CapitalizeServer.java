c8e299538a9f68764a583aadfe522e93b7795b8a
Duncan <silvos44@hotmail.fr>
2014-07-28 10:01:16 +0200

    MAJ server

--- src/CapitalizeServer.java
+++ src/CapitalizeServer.java
@@ -7,17 +7,7 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
-/**
- * A server program which accepts requests from clients to
- * capitalize strings.  When clients connect, a new thread is
- * started to handle an interactive dialog in which the client
- * sends in a string and the server thread sends back the
- * capitalized version of the string.
- *
- * The program is runs in an infinite loop, so shutdown in platform
- * dependent.  If you ran it from a console window with the "java"
- * interpreter, Ctrl+C generally will shut it down.
- */
+
 public class CapitalizeServer {
 	 static Robot robot ;
 	 
@@ -85,23 +75,12 @@ public class CapitalizeServer {
 	 static int[] iplayer3 = {allVK_KEYS[28],allVK_KEYS[29],allVK_KEYS[30],allVK_KEYS[31],allVK_KEYS[32],allVK_KEYS[33],allVK_KEYS[34],allVK_KEYS[35],allVK_KEYS[36],allVK_KEYS[37],allVK_KEYS[38],allVK_KEYS[39],allVK_KEYS[40],allVK_KEYS[41]};
 	 static int[] iplayer4 = {allVK_KEYS[42],allVK_KEYS[43],allVK_KEYS[44],allVK_KEYS[45],allVK_KEYS[46],allVK_KEYS[47],allVK_KEYS[48],allVK_KEYS[49],allVK_KEYS[50],allVK_KEYS[51],allVK_KEYS[52],allVK_KEYS[53],allVK_KEYS[54],allVK_KEYS[55]};
 	 
-//	 static char[] player1 = {'a','z','e','r','t','y','u','i','o','p','q','s','d','f'};
-//	 static char[] player2 = {'g','h','j','k','l','m','w','x','c','v','b','n',',',';'};
-//	 static char[] player3 = {'&','é','(','-','è','=','5','à',')','0','1','2','3','4'};
-	 //static char[] player4 = {'','','','','','','','','','','','','',''};
 	 static HashMap<String,Boolean> player_dispo;
 	 static boolean p1 = false;
 	 static boolean p2 = false;
 	 static boolean p3 = false;
 	 static boolean p4 = false;
-    /**
-     * Application method to run the server runs in an infinite loop
-     * listening on port 9898.  When a connection is requested, it
-     * spawns a new thread to do the servicing and immediately returns
-     * to listening.  The server keeps a unique client number for each
-     * client that connects just to show interesting logging
-     * messages.  It is certainly not necessary to do this.
-     */
+   
     public static void main(String[] args) throws Exception {
     	player_dispo = new HashMap<>();
     	player_dispo.put("a1", false);
@@ -131,11 +110,7 @@ public class CapitalizeServer {
         }
     }
 
-    /**
-     * A private thread to handle capitalization requests on a particular
-     * socket.  The client terminates the dialogue by sending a single line
-     * containing only a period.
-     */
+  
     private static class Capitalizer extends Thread {
         private Socket socket;
         private int clientNumber;
@@ -150,55 +125,38 @@ public class CapitalizeServer {
             log("New connection with client# " + clientNumber + " at " + socket + " " + player_dispo.get("a"+(this.clientNumber+1)));
         }
 
-        /**
-         * Services this thread's client by first sending the
-         * client a welcome message then repeatedly reading strings
-         * and sending back the capitalized version of the string.
-         */
+  
         public void run() {
             try {
 
-                // Decorate the streams so we can send characters
-                // and not just bytes.  Ensure output is flushed
-                // after every newline.
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(socket.getInputStream()));
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-                // Send a welcome message to the client.
                 out.println("Player " + (clientNumber+1));
 
-                // Get messages from the client, line by line; return them
-                // capitalized
                 while (true) {
                     String input = in.readLine();
                     if (input == null || input.equals(".")) {
                         break;
                     }
-//                    out.println(input.toUpperCase());
-//                    log("" + clientNumber + " say " + input);
-                    
+
                     	int ks = 0;
                     	if(input.contains("d")){
-//                    		log(clientNumber + " => " + (clientNumber%3));
                     		switch(clientNumber){
 	                    		case 0 : 
-//	                    			ks = KeyEvent.getExtendedKeyCodeForChar(player1[Integer.valueOf(input.replaceAll("d", ""))] );
 	                    			ks = iplayer1[Integer.valueOf(input.replaceAll("d", ""))];
 	                    			log("Player 1 : "+KeyEvent.getKeyText(ks));
 	                    			break;
 	                    		case 1 : 
-//	                    			ks = KeyEvent.getExtendedKeyCodeForChar(player2[Integer.valueOf(input.replaceAll("d", ""))] );
 	                    			ks = iplayer2[Integer.valueOf(input.replaceAll("d", ""))];
 	                    			log("Player 2 : "+KeyEvent.getKeyText(ks));
 	                    			break;
 	                    		case 2 : 
-//	                    			ks = KeyEvent.getExtendedKeyCodeForChar(player3[Integer.valueOf(input.replaceAll("d", ""))] );
 	                    			ks = iplayer3[Integer.valueOf(input.replaceAll("d", ""))];
 	                    			log("Player 3 : "+KeyEvent.getKeyText(ks));
 	                    			break;
 	                    		case 3 : 
-//	                    			ks = KeyEvent.getExtendedKeyCodeForChar(player3[Integer.valueOf(input.replaceAll("d", ""))] );
 	                    			ks = iplayer4[Integer.valueOf(input.replaceAll("d", ""))];
 	                    			log("Player 4 : "+KeyEvent.getKeyText(ks));
 	                    			break;
@@ -207,19 +165,15 @@ public class CapitalizeServer {
                     	}else if(input.contains("u")){
                     		switch(clientNumber){
                     		case 0 : 
-//                    			ks = KeyEvent.getExtendedKeyCodeForChar(player1[Integer.valueOf(input.replaceAll("u", ""))] );
                     			ks = iplayer1[Integer.valueOf(input.replaceAll("u", ""))];
                     			break;
                     		case 1 : 
-//                    			ks = KeyEvent.getExtendedKeyCodeForChar(player2[Integer.valueOf(input.replaceAll("u", ""))] );
                     			ks = iplayer2[Integer.valueOf(input.replaceAll("u", ""))];
                     			break;
                     		case 2 : 
-//                    			ks = KeyEvent.getExtendedKeyCodeForChar(player3[Integer.valueOf(input.replaceAll("u", ""))] );
                     			ks = iplayer3[Integer.valueOf(input.replaceAll("u", ""))];
                     			break;
                     		case 3 : 
-//                    			ks = KeyEvent.getExtendedKeyCodeForChar(player3[Integer.valueOf(input.replaceAll("u", ""))] );
                     			ks = iplayer4[Integer.valueOf(input.replaceAll("u", ""))];
                     			break;	
                     		}
@@ -236,10 +190,7 @@ public class CapitalizeServer {
                     		player_dispo.put("a"+(this.clientNumber+1), true);
                     		
                     	}
-                    		log( input );                    		
-//                    		robot.delay(10);                    		
-                    
-                   
+                    		log( input );
                 }
             } catch (IOException e) {
                 log("Error handling client# " + clientNumber + ": " + e);
@@ -253,13 +204,8 @@ public class CapitalizeServer {
                 player_dispo.put("a"+(clientNumber+1), false);
             }
         }
-
-        /**
-         * Logs a simple message.  In this case we just write the
-         * message to the server applications standard output.
-         */
         private void log(String message) {
-            System.out.println(message);
+            //System.out.println(message);
         }
     }
 }
